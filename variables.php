6b2c83d3291dcb1bd7d51f8c547db15950759c75
Santiago Ochoa Paipilla <santiagop200611@gmail.com>
2025-09-13 10:53:03 -0500

    Se difinieron las variables

---
+++ variables.php
@@ -0,0 +1,12 @@
+<a href="index.html">Index</a>
+<?php
+$nombre = 'Ana';
+$apellido = 'Gomez';
+
+
+echo "<br>".$nombre . " ". $apellido;
+$edad = 32;
+$promedio = 3.5;
+$mayorEdad = true;
+$ejemplo = null;
+?>
